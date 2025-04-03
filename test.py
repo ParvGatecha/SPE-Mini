9d031294bae8898027324a2591bba07b464ed063
ParvGatecha <gatechaparv@gmail.com>
2025-04-03 10:57:10 +0530

    test check

--- test.py
+++ test.py
@@ -8,6 +8,9 @@ def test_square_root():
     response = client.get("/sqrt/-9")
     assert response.status_code == 200
     assert response.json()["result"] == "Enter valid number"
+    response = client.get("/sqrt/9")
+    assert response.status_code == 200
+    assert response.json()["result"] == 3
 
 def test_factorial():
     response = client.get("/factorial/5")
